e990d55979358b9e1e7c9aa27ce02076363c567d
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-05 09:37:49 +0700

    Add Footer Component

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -1,6 +1,7 @@
 import { type ReactNode } from 'react'
 
 import { Navbar } from '../components/storefront/Navbar'
+import { Footer } from '../components/storefront/Footer'
 
 export default function StoreFrontLayout({
   children,
@@ -11,6 +12,7 @@ export default function StoreFrontLayout({
     <>
       <Navbar />
       <main className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>{children}</main>
+      <Footer />
     </>
   )
 }
