108dd3d7fef20ca29c84de2ab17f3e65cd5171a8
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-06 08:16:50 +0700

    Create BagRoute

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -53,3 +53,38 @@ export function ShoppingBagButton() {
     </>
   )
 }
+
+export function DeleteItem() {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      {pending ? (
+        <button disabled className='font-medium text-primary text-end'>
+          Removing...
+        </button>
+      ) : (
+        <button type='submit' className='font-medium text-primary text-end'>
+          Delete
+        </button>
+      )}
+    </>
+  )
+}
+
+export function CheckoutButton() {
+  const { pending } = useFormStatus()
+  return (
+    <>
+      {pending ? (
+        <Button disabled size='lg' className='w-full mt-5'>
+          <Loader2 className='mr-2 h-5 w-5 animate-spin' /> Please Wait
+        </Button>
+      ) : (
+        <Button type='submit' size='lg' className='w-full mt-5'>
+          Checkout
+        </Button>
+      )}
+    </>
+  )
+}
