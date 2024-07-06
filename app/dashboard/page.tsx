82ee3eb2fc6964ecd32a82747e96ee1e7ba5d649
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-06 11:53:33 +0700

    Add Chart Component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
+import { Chart } from '../components/dashboard/Chart'
 import { DashboardStats } from '../components/dashboard/DashboardStats'
+import { RecentSales } from '../components/dashboard/RecentSales'
 import {
   Card,
   CardContent,
@@ -6,8 +8,39 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import prisma from '../lib/db'
+
+async function getData() {
+  const now = new Date()
+  const sevenDaysAgo = new Date()
+  sevenDaysAgo.setDate(now.getDate() - 7)
+
+  const data = await prisma.order.findMany({
+    where: {
+      createdAt: {
+        gte: sevenDaysAgo,
+      },
+    },
+    select: {
+      amount: true,
+      createdAt: true,
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  })
+
+  const result = data.map((item) => ({
+    date: new Intl.DateTimeFormat('en-US').format(item.createdAt),
+    revenue: item.amount / 100,
+  }))
+
+  return result
+}
 
 export default async function Dashboard() {
+  const data = await getData()
+
   return (
     <>
       <DashboardStats />
@@ -19,9 +52,11 @@ export default async function Dashboard() {
               Recent transactions from the last 7 days
             </CardDescription>
           </CardHeader>
-          <CardContent>Chart</CardContent>
+          <CardContent>
+            <Chart data={data} />
+          </CardContent>
         </Card>
-        RecentSales
+        <RecentSales />
       </div>
     </>
   )
